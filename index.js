4a42b52400809aaee8c9a067b1311cde729bb682
robiuljoy <robiuljoy91@gmail.com>
2025-11-09 23:16:09 +0600

    get single event

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Server is running fine");
-});
-
 const uri =
   "mongodb+srv://socialDevelopmentServer:R2oQAWCPwqJecrY7@cluster0.hl8gbtt.mongodb.net/?appName=Cluster0";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -28,26 +24,30 @@ async function run() {
     await client.connect();
 
     const db = client.db("social_work_db");
-    const mainCollection = db.collection("main");
+    const eventsCollection = db.collection("events");
+    const joinedEventsCollection = db.collection("joinedEvents");
 
-    app.get("/main", async (req, res) => {
-      const result = await mainCollection.find().toArray();
-
+    app.get("/events", async (req, res) => {
+      const result = await eventsCollection.find().toArray();
       res.send(result);
     });
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    app.get("/events/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await eventsCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    console.log("MongoDB Pinged Successfully");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Social Development Events Server is Running...");
+});
+
 app.listen(port, () => {
-  console.log(`Server is running on post ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
